dd2cf0e8406029cd72ad1193ce5ec748102e751b
Lukasz Wisowaty <lukaszwisowaty@gmail.com>
2023-06-12 09:48:39 +0200

    przyciski vinfo

--- resources/views/livewire/user/occupant/show-verbrauchsinfo.blade.php
+++ resources/views/livewire/user/occupant/show-verbrauchsinfo.blade.php
@@ -1,22 +1,22 @@
 <div>
-    <div class="max-w-7xl w-full mx-auto py-1 px-4 sm:px-6 lg:px-8">
+    <div class="w-full px-4 py-1 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div>
             <x-input.search wire:model.debounce.1000ms="filter" />
         </div>
-        <div class="mt-6 w-full grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div class="grid w-full grid-cols-1 gap-4 mt-6 sm:grid-cols-2 lg:grid-cols-3">
             @if ($rows->count()!=0)
                @foreach ($rows as $occupant)
                 <div>
-                    <div x-data="{ open: false }" class="max-w-1/4 col-span-1 bg-sky-100 shadow-md divide-y divide-gray-200 rounded-lg">
+                    <div x-data="{ open: false }" class="col-span-1 divide-y divide-gray-200 rounded-lg shadow-md max-w-1/4 bg-sky-100">
                         <div class="flex items-center justify-between w-full p-2 space-x-6 ">
-                            <div class="flex-1 truncate  border-sky-200 ">
+                            <div class="flex-1 truncate border-sky-200 ">
                                 <div x-on:click="open = ! open"  class="w-full">
                                     <div>
                                         <div class="flex items-center space-x-3 ">
-                                            <h3 class="flex-1 text-center line-clamp-1 text-lg text-gray-900 truncate font-mdmedium text- md:font-bold md:text-md">{{ $occupant->lage. ' - '. $occupant->nachname. '' }}</h3>
+                                            <h3 class="flex-1 text-lg text-center text-gray-900 truncate line-clamp-1 font-mdmedium text- md:font-bold md:text-md">{{ $occupant->lage. ' - '. $occupant->nachname. '' }}</h3>
                                         </div>
                                     </div>
-                                    <p class="mt-1 flex-1 text-center text-gray-500 truncate text-md">{{ $occupant->street.', '. $occupant->postcode. ' '. $occupant->city }}</p>
+                                    <p class="flex-1 mt-1 text-center text-gray-500 truncate text-md">{{ $occupant->street.', '. $occupant->postcode. ' '. $occupant->city }}</p>
                                 </div>
                                 <div x-show="open" x-transition>
                                     <div class="my-5">
@@ -24,35 +24,20 @@
                                     </div>
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
+                    
                 </div>
+                
                 @endforeach
             @endif
         </div>
-        <x-button.transparent class="flex-row bg-sky-100 px-2 py-1 mt-3" :active="request()->routeIs('user.verbrauchsinfohistory')">
-            <a href="{{route('user.occupantVerbrauchsinfos', $occupant)}}">
-                <span class = "text-md font-semibold text-black opacity-90 group-hover:opacity-100 transition duration-150 ease">
-                    {{ __('Verlaufsliste') }}
-                </span>
-            </a>
-        </x-button.transparent>
-
-        <x-button.transparent class="flex-row bg-sky-100 px-2 py-1 mt-3" :active="request()->routeIs('user.occupantVerbrauchsinfoCounterMeters')">
-            <a href="{{route('user.occupantVerbrauchsinfoCounterMeters', ['occupant_id' => $occupant, 'jahr_monat' => '2023-6'])}}">
-
-                <span class = "text-md font-semibold text-black opacity-90 group-hover:opacity-100 transition duration-150 ease">
-
-                    {{ __('Zähler anzeigen') }}
-
-                </span>
-
-            </a>
-            </x-button.transparent>
+ 
 
         @if ($rows->count()==0)
-            <div class="flex justify-center items-center space-x-2 bg-sky-100">
-                <span class="font-medium py-8 text-cool-gray-400 text-xl">nichts gefunden...</span>
+            <div class="flex items-center justify-center space-x-2 bg-sky-100">
+                <span class="py-8 text-xl font-medium text-cool-gray-400">nichts gefunden...</span>
             </div>
         @endif
     </div>
--- resources/views/livewire/user/occupant/verbrauchsinfo/occupant-view.blade.php
+++ resources/views/livewire/user/occupant/verbrauchsinfo/occupant-view.blade.php
@@ -1,45 +1,69 @@
 <div>
-    <div class="mt-6 w-full">
+    <div class="w-full mt-6">
         @if ($rows->count()!=0)
             @foreach ($rows as $verbrauchsinfo)
             <div class="mt-6 {{ $verbrauchsinfo->ww ? 'border-y-2 border-red-600' : 'border-y-2 border-green-400' }} selection:items-center justify-between w-full p-2 ">
                 <div class="flex-1 text-center font-semibold {{ $verbrauchsinfo->ww ? 'text-red-800 ' : 'text-green-600 ' }} ">
                     {{ $verbrauchsinfo->art. ' im '. $verbrauchsinfo->zeitraum_akt}}
                 </div>
-                <div class="flex-1 py-3 text-center font-semibold  ">
+                <div class="flex-1 pt-4 font-semibold text-center p3 ">
                     Verbrauchswerte
                 </div>
-                <div class="w-full grid gap-4 sx:grid-cols-3">
+                <div class="grid w-full gap-4 sx:grid-cols-3">
                     <div class="flex justify-between">
                         <div class="">
-                            <div class="flex-1 text-sm text-gray-600 underline text-center"> {{ $verbrauchsinfo->zeitraum_akt}} </div>
+                            <div class="flex-1 text-sm text-center text-gray-600 underline"> {{ $verbrauchsinfo->zeitraum_akt}} </div>
                             <div class="flex-1 text-center"> {{ $verbrauchsinfo->verbrauch_akt_display}} </div>
                         </div>
                         <div class="">
-                            <div class="flex-1 text-sm text-gray-600 underline text-center"> {{ $verbrauchsinfo->zeitraum_mon}} </div>
+                            <div class="flex-1 text-sm text-center text-gray-600 underline"> {{ $verbrauchsinfo->zeitraum_mon}} </div>
                             <div class="flex-1 text-center"> {{ $verbrauchsinfo->verbrauch_mon_display}} </div>
                          </div>
                         <div class="">
-                            <div class="flex-1 text-sm text-gray-600 underline text-center"> {{ $verbrauchsinfo->zeitraum_vorj}} </div>
+                            <div class="flex-1 text-sm text-center text-gray-600 underline"> {{ $verbrauchsinfo->zeitraum_vorj}} </div>
                             <div class="flex-1 text-center"> {{ $verbrauchsinfo->verbrauch_vorj_display}} </div>
                           </div>
                     </div>
                 </div>
-                <div class="flex-1  pt-3 text-center font-semibold  ">
+                <div class="flex-1 pt-3 font-semibold text-center ">
                     Gebäudedurchschnitt
                 </div>
+                
                 <div class="flex-1 text-center">
                     {{ $verbrauchsinfo->durchschnitt_display }}
                 </div>
+                
             </div>
 
             @endforeach
+            <div class="flex justify-around">
+            <x-button.transparent class="flex-row px-2 py-1 mt-3 bg-sky-100 border-1 border-sky-200" :active="request()->routeIs('user.verbrauchsinfohistory')">
+                <a href="{{route('user.occupantVerbrauchsinfos', $occupant)}}">
+                    <span class = "font-semibold text-black transition duration-150 text-md opacity-90 group-hover:opacity-100 ease">
+                        {{ __('Verlaufsliste') }}
+                    </span>
+                </a>
+            </x-button.transparent>
+    
+            <x-button.transparent class="flex-row px-2 py-1 mt-3 bg-sky-100" :active="request()->routeIs('user.occupantVerbrauchsinfoCounterMeters')">
+                <a href="{{route('user.occupantVerbrauchsinfoCounterMeters', ['occupant_id' => $occupant, 'jahr_monat' => '2023-6'])}}">
+    
+                    <span class = "font-semibold text-black transition duration-150 text-md opacity-90 group-hover:opacity-100 ease">
+    
+                        {{ __('Zähler anzeigen') }}
+    
+                    </span>
+    
+                </a>
+                </x-button.transparent>
+            </div>
         @endif
+
     </div>
 
     @if ($rows->count()==0)
-        <div class="flex justify-center items-center space-x-2 bg-sky-100">
-            <span class="font-medium py-8 text-cool-gray-400 text-xl">nichts gefunden...</span>
+        <div class="flex items-center justify-center space-x-2 bg-sky-100">
+            <span class="py-8 text-xl font-medium text-cool-gray-400">nichts gefunden...</span>
         </div>
     @endif
 
