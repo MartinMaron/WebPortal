9848f80d70aee2e1035432bcced04ae51708b468
MartinMaron <martin.maron@e-neko.de>
2023-08-29 17:31:16 +0200

    Import Rachunkow

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,15 +2,90 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Invoice extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'createDate', 'caption', 'description', 'fileName', 'dateFrom', 'dateTo', 'nekoId'
+        'createDate', 'caption', 'description', 'fileName', 'dateFrom', 'dateTo', 'nekoId', 'realestate_id',
+        'vertragsart','bezahlt','bezahltAm','zahlungsAuftragDatum','zahlungsauftragIBAN','netto','vat','brutto'
     ];
+
+    public function realestate()
+    {
+        return $this->belongsTo(Realestate::class);
+    }
+
+
+    public static function validateImportData($data)
+    {
+        return  Validator::make($data, [
+            'nekoId' => 'required|integer',
+            'createDate' => 'required|date', 
+            'caption' => 'required|string|max:255', 
+            'description' => 'sometimes',  
+            'fileName' => 'sometimes', 
+            'dateFrom' => 'required|date', 
+            'dateTo' => 'required|date', 
+            'vertragsart' => 'required|string|max:255',
+            'bezahlt' => 'required|boolean',
+            'bezahltAm' => 'sometimes',
+            'zahlungsAuftragDatum' => 'sometimes',
+            'zahlungsauftragIBAN' => 'sometimes',
+            'netto' => 'required|numeric',
+            'vat' => 'required|numeric',
+            'brutto' => 'required|numeric',
+        ]);
+    }
+
+    protected $casts = ['dateFrom' => 'date:d.m.Y',
+                        'dateTo' => 'date:d.m.Y',
+                        'createDate' => 'date:d.m.Y', ];
+
+    
+    protected $appends = ['date_from_editing',
+                          'date_to_editing',
+                          'createDate_editing'];
+
+    public function getDateFromEditingAttribute()
+    {
+        return Carbon::parse($this->dateFrom)->format('d.m.Y');
+    }
+
+    public function setDateFromEditingAttribute($value)
+    {
+        $this->dateFrom = Carbon::parse($value);
+    }
+
+    public function getDateToEditingAttribute()
+    {
+        if($this->dateTo){
+            return Carbon::parse($this->dateTo)->format('d.m.Y');
+        }
+        return '';
+    }
+
+    public function setDateToEditingAttribute($value)
+    {
+        $this->dateTo = Carbon::parse($value);
+    }
+
+    public function getCreateDateEditingAttribute()
+    {
+        if($this->dateTo){
+            return Carbon::parse($this->dateTo)->format('d.m.Y');
+        }
+        return '';
+    }
+
+    public function setCreateDateEditingAttribute($value)
+    {
+        $this->dateTo = Carbon::parse($value);
+    }
 
 }
