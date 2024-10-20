27dbdf76ca0df28060627565f55f7c78ff87b157
leartpro <70581914+leartpro@users.noreply.github.com>
2024-10-20 18:17:26 +0200

    dynamic import ToastComponents

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap.js';
 
 import Alpine from 'alpinejs';
 import mask from '@alpinejs/mask'
-const ToastComponent = require('../../vendor/usernotnull/tall-toasts/dist/js/tall-toasts');
-
-Alpine.data('ToastComponent', ToastComponent)
-
+import('../../vendor/usernotnull/tall-toasts/dist/js/tall-toasts.js')
+    .then(() => {
+        // After loading, set the ToastComponent to Alpine
+        Alpine.data('ToastComponent', window.Toast); // Use the global Toast object
+    })
+    .catch((error) => {
+        console.error('Failed to load ToastComponent:', error); // Handle errors
+    });
 Alpine.plugin(mask)
-
 window.Alpine = Alpine;
-
 Alpine.start();
