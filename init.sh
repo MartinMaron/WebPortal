8f4291f4dae7e7c3cf8acc56dc628fb0d1e75d29
leartpro <70581914+leartpro@users.noreply.github.com>
2024-11-10 14:40:17 +0100

    cleanup

---
+++ init.sh
@@ -0,0 +1,13 @@
+brew services restart php@8.3
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+cp .env.example .env
+npm install
+composer install
+php artisan key:generate
+php artisan migrate:refresh --seed
+npm run prod
+npm run dev
+php artisan serve
